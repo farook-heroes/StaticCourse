4721f555397bf72c13885d90cb0b055698ac254b
farook-heroes <farookheroes@gmail.com>
2023-10-24 09:41:58 +0530

    Onborarding Work started

--- home/views.py
+++ home/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth import logout
 
 from django.contrib.auth.decorators import login_required
 
+
+from django.contrib.auth.models import User, Group
+
+
 from .models import *
 
 def index(request):
@@ -32,8 +36,25 @@ def sample_page(request):
 
 @login_required(login_url='/accounts/login/')
 def tab_page(request):
-  
-  context = {
+    if request.method == 'POST':
+        print("hello")
+        username=request.POST["username"]
+        password=request.POST["password"]
+        role=request.POST['role']
+        print(request)
+        if role == 'student':
+            student_group = Group.objects.get(name='Student')
+            student_user = User.objects.create_user(username=username, password=password)
+            
+            student_user.groups.add(student_group)
+            student_user.save()
+        elif role == 'teacher':
+            teacher_group = Group.objects.get(name='Teacher')
+            teacher_user = Teacher(username=username, password=password)
+            teacher_user.save()
+            teacher_user.groups.add(teacher_group)
+        
+    context = {
     'segment': 'tab_page',
   }
-  return render(request, 'pages/tabs.html', context)
+    return render(request, 'pages/tabs.html', context)
