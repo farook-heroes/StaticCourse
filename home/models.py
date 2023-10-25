2c3b5f62e767adf09684d63a8c6ffb8637112337
farook-heroes <farookheroes@gmail.com>
2023-10-25 11:18:01 +0530

    Onboarding model created

--- home/models.py
+++ home/models.py
@@ -12,19 +12,34 @@ class Product(models.Model):
         return self.name
 
 
+
 class Student(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, unique=True)
-    subject = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    about_info = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
+    
+class Teacher(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    about_info = models.TextField(blank=True)
 
     def __str__(self):
         return self.username
 
-class Booking(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='bookings')
-    booking_time = models.DateTimeField()
-    subject = models.CharField(max_length=100)
 
-    def __str__(self):
-        return f"{self.student.username}'s Booking"
--- home/views.py
+++ home/views.py
@@ -45,14 +45,61 @@ def tab_page(request):
         if role == 'student':
             student_group = Group.objects.get(name='Student')
             student_user = User.objects.create_user(username=username, password=password)
-            
+            username = request.POST['username']
+            password = request.POST['password']
+            email = request.POST['email']
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            phone_number = request.POST['phone_number']
+            age = request.POST['age']
+            address = request.POST['address']
+            about_info = request.POST['about_info']
+
+            # Create a Student object and save it to the model
+            student = Student(
+                username=username,
+                password=password,
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+                phone_number=phone_number,
+                age=age,
+                address=address,
+                about_info=about_info
+            )
+            student.save()
             student_user.groups.add(student_group)
             student_user.save()
         elif role == 'teacher':
             teacher_group = Group.objects.get(name='Teacher')
-            teacher_user = Teacher(username=username, password=password)
+            teacher_user = User.objects.create_user(username=username, password=password)
             teacher_user.save()
             teacher_user.groups.add(teacher_group)
+            username = request.POST['username']
+            password = request.POST['password']
+            email = request.POST['email']
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            phone_number = request.POST['phone_number']
+            subject = request.POST['subject']
+            age = request.POST['age']
+            address = request.POST['address']
+            about_info = request.POST['about_info']
+
+            # Create a Teacher object and save it to the model
+            teacher = Teacher(
+                username=username,
+                password=password,
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+                phone_number=phone_number,
+                subject=subject,
+                age=age,
+                address=address,
+                about_info=about_info
+            )
+            teacher.save()
         
     context = {
     'segment': 'tab_page',
